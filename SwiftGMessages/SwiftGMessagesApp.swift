42edda065ac6ea1665dfeddc19306e6adfa9fb45
mweinbach <maxweinbach5@gmail.com>
2025-12-06 23:21:09 -0500

    Initial Commit

---
+++ SwiftGMessages/SwiftGMessagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGMessagesApp.swift
+//  SwiftGMessages
+//
+//  Created by Max Weinbach on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGMessagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
